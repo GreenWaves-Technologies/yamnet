622f0d7aa72238651002613fa930c62b580825cd
Germain Haugou <germain.haugou@greenwaves-technologies.com>
2022-04-12 08:31:12 +0200

    Added missing cluster_task init

--- yamnet.c
+++ yamnet.c
@@ -70,6 +70,7 @@ int test_yamnet(void)
     struct pi_cluster_conf cl_conf = {0};
     pi_cluster_conf_init(&cl_conf);
     cl_conf.id = 0;
+    cl_conf.cc_stack_size = STACK_SIZE;
     pi_open_from_conf(&cluster_dev, (void *) &cl_conf);
     if (pi_cluster_open(&cluster_dev))
     {
@@ -89,12 +90,9 @@ int test_yamnet(void)
 
     printf("Call cluster\n");
 #ifndef __EMUL__
-    struct pi_cluster_task task = {0};
-    task.entry = cluster;
-    task.arg = NULL;
-    task.stack_size = (unsigned int) STACK_SIZE;
-    task.slave_stack_size = (unsigned int) SLAVE_STACK_SIZE;
-
+    struct pi_cluster_task task;
+    pi_cluster_task(&task, (void (*)(void *))cluster, NULL);
+    pi_cluster_task_stacks(&task, NULL, SLAVE_STACK_SIZE);
     pi_cluster_send_task_to_cl(&cluster_dev, &task);
 #else
     cluster();
